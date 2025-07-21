4f256d7e391519bd13b1b271fb8838bd33bbd80d
hammadkhan <86566345+hammadkhan87@users.noreply.github.com>
2025-07-21 23:36:43 +0400

    abcd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
-
+import {Header} from "@/sections/Header"
+import { Hero } from "@/sections/Hero";
 export default function Home() {
   return (
-    
-         <div>
-          <h1>Home</h1>
-         </div>
+    <>
+    <Header/>
+    <Hero/>
+    </>
   );
 }
