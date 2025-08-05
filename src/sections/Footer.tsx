0dac6673d401e91538241a0dfb6686f46d684654
hammadkhan <86566345+hammadkhan87@users.noreply.github.com>
2025-08-05 16:23:38 +0400

    fg

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,15 +1,18 @@
+"use client";
 import logo from "@/assets/logosaas.png"
 import Image from "next/image";
-import socialX from "@/assets/social-x.svg"
-import socialInsta from "@/assets/social-insta.svg"
-import socialLinkedind from "@/assets/social-linkedin.svg"
-import socialPin  from "@/assets/social-pin.svg"
-import socialYoutube from "@/assets/social-youtube.svg"
+import SocialX from "@/assets/social-x.svg"
+import SocialInsta from "@/assets/social-insta.svg"
+import SocialLinkedind from "@/assets/social-linkedin.svg"
+import SocialPin  from "@/assets/social-pin.svg"
+import SocialYoutube from "@/assets/social-youtube.svg"
 export const Footer = () => {
-  return <footer>
+  return <footer className="bg-black text-[#BCBCBC] text-sm py-10 text-center">
       <div className="container">
-       <Image src={logo} alt="logo" height={40} />
-       <nav>
+        <div className="inline-flex relative before:content-[''] before:top-2 before:bottom-0 before:h-full before:w-full before:blur before:bg-[linear-gradient(to_right,#F87BFF,#FB92CF,#FFDD9B,C2F0B1,#2FD8FE)] before:absolute">
+       <Image src={logo} alt="logo" height={40} className="relative" />
+       </div>
+       <nav className="flex md:flex-row md:justify-center flex-col gap-6 mt-6">
         <a href="#">About</a>
         <a href="#">Features</a>
         <a href="#">Customer</a>
@@ -17,6 +20,14 @@ export const Footer = () => {
         <a href="#">Help</a>
         <a href="#">Careers</a>
        </nav>
+       <div className="flex justify-center gap-6 mt-6">
+        <SocialX/>
+        <SocialInsta/>
+        <SocialLinkedind/>
+        <SocialPin/>
+        <SocialYoutube/>
+       </div>
+       <p className="mt-6">&copy; 2025 Your Company , Inc. All rights reserved</p>
       </div>
   </footer>;
 };
