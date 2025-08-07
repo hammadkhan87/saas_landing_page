c4ab2fd7129c0cb076282b422e1717a0934f3ed5
hammadkhan <86566345+hammadkhan87@users.noreply.github.com>
2025-08-07 04:44:46 +0400

    cd

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -10,7 +10,7 @@ export const Footer = () => {
   return <footer className="bg-black text-[#BCBCBC] text-sm py-10 text-center">
       <div className="container">
         <div className="inline-flex relative before:content-[''] before:top-2 before:bottom-0 before:h-full before:w-full before:blur before:bg-[linear-gradient(to_right,#F87BFF,#FB92CF,#FFDD9B,C2F0B1,#2FD8FE)] before:absolute">
-       <Image src={logo} alt="logo" height={40} className="relative" />
+       <Image src={logo} alt="log" height={40} className="relative" />
        </div>
        <nav className="flex md:flex-row md:justify-center flex-col gap-6 mt-6">
         <a href="#">About</a>
